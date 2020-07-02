e41982b64830b0bf3e2b8e213e3d1e69d97e8b23
Arthur <immola.chr.arthur@dxc.com>
2020-07-02 19:02:02 +0530

    5th commit

--- terraform.sh
+++ terraform.sh
@@ -1,8 +1,8 @@
 # Install the AWS provider plugin
-terraform int
+terraform init
 
 # Dry-run check
 terraform plan
 
 # Provision the infrastructure
-terraform apply?--var-file=variables.tfvars
+terraform apply --var-file=variables.tfvars
